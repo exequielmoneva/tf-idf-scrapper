f5aa591be0b94662799bfe35b1668d8e33e9c6ea
exequielmoneva <exequielmoneva@gmail.com>
2020-12-01 18:01:11 -0300

    Code refactor

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'API started'
+    return 'API has started'
 
 
 @app.route('/tfidf')
 def url_reader():
-    url = request.args.get('url')
-    return Scrapping.get_html_data_service(url)
-# return jsonify(jresponse.dump(Scrapping.get_html_data_service(url)))
+    query_params = request.args.to_dict()
+    eval = Scrapping.get_html_data_service(query_params)
+    return eval
+
 
 if __name__ == '__main__':
     app.run()
--- services/scrapping_service.py
+++ services/scrapping_service.py
@@ -1,10 +1,12 @@
 import re
+from heapq import nlargest
+
 import nltk
-import numpy as np
 import requests
 from bs4 import BeautifulSoup
+from flask import jsonify
 from nltk.tokenize import sent_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -13,22 +15,15 @@ nltk.download('wordnet')
 
 class ProcessingData:
     @classmethod
-    def process_sentences(cls, data):
-        tfidf_vectorizer = TfidfVectorizer(
-            analyzer='word', min_df=0, stop_words='english', sublinear_tf=True,
-            use_idf=True)
-        tfidf_vectorizer_vectors = tfidf_vectorizer.fit_transform(data)
-        tfidf = tfidf_vectorizer_vectors.todense()
-        tfidf[tfidf == 0] = np.nan
-        means = np.nanmean(tfidf, axis=0)
-        means = dict(zip(tfidf_vectorizer.get_feature_names(), np.round(means.tolist()[0], 2)))
-        tfidf = tfidf_vectorizer_vectors.todense()
-        ordered = np.argsort(tfidf * -1)
-        words = tfidf_vectorizer.get_feature_names()
-        for i, doc in enumerate(data):
-            result = {}
-            for t in range(len(words)):
-                result[words[ordered[i, t]]] = means[words[ordered[i, t]]]
+    def process_sentences(cls, data, limit):
+        cv = CountVectorizer()
+        word_count_vector = cv.fit_transform(data)
+        tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+        tfidf_transformer.fit(word_count_vector)
+        terms = [{'term': term, 'value': round(value, 2)} for term, value in
+                 zip(cv.get_feature_names(), tfidf_transformer.idf_)
+                 ]
+        result = nlargest(limit, terms, key=lambda item: item["value"])
         return result
 
 
@@ -37,6 +32,7 @@ class Scrapping:
     def clean_data(cls, text):
         corpus = sent_tokenize(text)
         for i in range(len(corpus)):
+            corpus[i] = ''.join(c for c in corpus[i] if not c.isdigit())
             corpus[i] = corpus[i].lower()
             corpus[i] = re.sub(r'\W', ' ', corpus[i])
             corpus[i] = re.sub(r'\s+', ' ', corpus[i])
@@ -52,7 +48,24 @@ class Scrapping:
         return cls.clean_data(article_text)
 
     @classmethod
-    def get_html_data_service(cls, url):
-        page = requests.get(url)
-        cleaned_sentences = cls.parse_body(page.content)
-        return ProcessingData.process_sentences(cleaned_sentences)
+    def get_html_data_service(cls, params):
+        try:
+            page = requests.get(params.get('url'))
+            cleaned_sentences = cls.parse_body(page.content)
+            return jsonify(terms=ProcessingData.process_sentences(
+                cleaned_sentences, int(params.get('limit')))
+            )
+        except TypeError:
+            if params.get('url') == None:
+                return jsonify(error='url is mandatory')
+            elif params.get('limit') == None:
+                return jsonify(error='limit is mandatory')
+        except ValueError as e:
+            if params.get('limit') == None:
+                return jsonify(error='missing url and limit parameters')
+            elif params.get('limit').isdigit():
+                return jsonify(error=str(e))
+            return jsonify(error='limit must be a number')
+        except Exception:
+            return jsonify(
+                error='invalid url')
